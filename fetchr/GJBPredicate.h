9403ae47bcd6befc0fd84374103a3aa75ef98912
Grant J. Butler <butler.grant.j@gmail.com>
2015-02-05 19:41:39 -0500

    Add GJBPredicate.

---
+++ fetchr/GJBPredicate.h
@@ -0,0 +1,18 @@
+//
+//  GJBPredicate.h
+//  fetchr
+//
+//  Created by Grant Butler on 2/5/15.
+//  Copyright (c) 2015 Grant Butler. All rights reserved.
+//
+
+@import Foundation;
+@import JavaScriptCore;
+
+@protocol GJBPredicate <JSExport>
+
+@end
+
+@interface GJBPredicate : NSObject <GJBPredicate>
+
+@end
---
+++ fetchr/GJBPredicate.m
@@ -0,0 +1,13 @@
+//
+//  GJBPredicate.m
+//  fetchr
+//
+//  Created by Grant Butler on 2/5/15.
+//  Copyright (c) 2015 Grant Butler. All rights reserved.
+//
+
+#import "GJBPredicate.h"
+
+@implementation GJBPredicate
+
+@end
