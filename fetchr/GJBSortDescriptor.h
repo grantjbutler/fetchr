bfc6d0db3fb8c7de99ddc23fcea88ace18e12ea7
Grant J. Butler <grant.butler@nytimes.com>
2015-02-11 09:57:19 -0500

    Add GJBSortDescriptor.

---
+++ fetchr/GJBSortDescriptor.h
@@ -0,0 +1,13 @@
+//
+//  GJBSortDescriptor.h
+//  fetchr
+//
+//  Created by Butler, Grant on 2/11/15.
+//  Copyright (c) 2015 Grant Butler. All rights reserved.
+//
+
+@import Foundation;
+
+@interface GJBSortDescriptor : NSObject
+
+@end
---
+++ fetchr/GJBSortDescriptor.m
@@ -0,0 +1,13 @@
+//
+//  GJBSortDescriptor.m
+//  fetchr
+//
+//  Created by Butler, Grant on 2/11/15.
+//  Copyright (c) 2015 Grant Butler. All rights reserved.
+//
+
+#import "GJBSortDescriptor.h"
+
+@implementation GJBSortDescriptor
+
+@end
